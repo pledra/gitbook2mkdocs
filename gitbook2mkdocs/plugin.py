2596a70cf3e97ffd0e6db0f56e1fc8a28ff2375f
Paul Catinean <pca@pledra.com>
2023-07-16 17:23:21 +0300

    Initial commit

---
+++ gitbook2mkdocs/plugin.py
@@ -0,0 +1,112 @@
+import os
+import re
+import shutil
+
+from mkdocs.plugins import BasePlugin
+
+
+class Gitbook2Mkdocs(BasePlugin):
+    def on_page_markdown(self, markdown, page, config, files):
+        # Replace GitBook syntax with MkDocs syntax
+        markdown = self.replace_gitbook_syntax(markdown)
+
+        # Replace HTML figures with Markdown images
+        markdown = self.replace_figures_with_images(markdown)
+
+        # Replace all references to .gitbook directory with assets
+        markdown = markdown.replace(
+            ".gitbook/assets/", self.get_relative_path_to_assets(page)
+        )
+
+        return markdown
+
+    def on_post_build(self, config):
+        # Define the source directory (.gitbook/assets)
+        source_dir = os.path.join(config["docs_dir"], ".gitbook", "assets")
+
+        # Define the destination directory (site/assets/images)
+        dest_dir = os.path.join(config["site_dir"], "assets", "gbtomk")
+
+        # If the source directory exists, copy it to the destination
+        if os.path.exists(source_dir):
+            shutil.copytree(source_dir, dest_dir, dirs_exist_ok=True)
+
+        # No need to replace .gitbook/assets/ with assets/ in HTML files
+        # because that's handled in on_page_markdown
+
+    def get_relative_path_to_assets(self, page):
+        # Count the number of slashes in the page URL to determine its depth
+        depth = page.file.url.count("/") - 1
+        # Construct a relative path to the assets directory based on the depth
+        return "../" * depth + "assets/gbtomk/"
+
+    # Function to replace GitBook syntax with MkDocs Admonition syntax
+    def replace_gitbook_syntax(self, content):
+        # Remove all gitbook specific [^1] values
+        content = re.sub(r"\[\^1\]:?", "", content)
+
+        # Remove content-ref tags and keep only the text between the tags
+        content = re.sub(r"{% content-ref %}(.*?){% endcontent-ref %}", r"\1", content)
+
+        # Remove content-ref tags
+        content = re.sub(
+            r"{%[\w\s]+?content-ref\s*.*?%}(.*?)\{%[\w\s]+?endcontent-ref\s*.*?%}",
+            r"\1",
+            content,
+            flags=re.DOTALL,
+        )
+
+        content = self.convert_hints(content)
+        content = self.convert_tabs(content)
+        return content
+
+    # Replace gitbook hint blocks with admonition
+    def convert_hints(self, content):
+        def indent_text(match):
+            hint_style = match.group(1)
+            inner_text = match.group(2).rstrip()
+            indented_text = inner_text.replace("\n", "\n    ")
+            return f"!!! {hint_style}\n    {indented_text}\n"
+
+        pattern = r'{% hint style="([a-zA-Z]+)" %}\s*(.*?)\s*{% endhint %}'
+        return re.sub(pattern, indent_text, content, flags=re.DOTALL)
+
+    # Function to change gitbooks tab format into mkdocs tab format
+    def convert_tabs(self, content):
+        # Replace {% tabs %} with an empty string (not needed in the target format)
+        content = re.sub(r"{% tabs %}", "", content)
+
+        # Replace {% endtabs %} with an empty string (not needed in the target format)
+        content = re.sub(r"{% endtabs %}", "", content)
+
+        # Replace {% tab title="Title" %} with the respective tab title
+        content = re.sub(r'{% tab title="([^"]+)" %}', r'=== "\1"', content)
+
+        # Replace {% endtab %} with an empty string (not needed in the target format)
+        content = re.sub(r"{% endtab %}", "", content)
+
+        # Indent content within tabs
+        tab_content_pattern = r'(=== ".*?")((?:\r?\n)(?:(?!\r?\n===).)*)(\r?\n)'
+
+        def indent_content(match):
+            title = match.group(1)
+            content = "    " + match.group(2).strip().replace("\n", "\n    ")
+            return "{}\n{}\n".format(title, content)
+
+        content = re.sub(tab_content_pattern, indent_content, content)
+
+        return content
+
+    def replace_figures_with_images(self, content):
+        pattern = r'<figure><img src="(.*?)" alt="(.*?)"><figcaption><p>.*?</p></figcaption></figure>'
+        replacement = r"![\2](\1)"
+        content = re.sub(pattern, replacement, content)
+        return content
+
+    # Function to remove escaping characters between colons
+    def remove_escaping_chars(self, content):
+        def process_match(match):
+            return match.group(1) + match.group(2).replace("\\", "") + match.group(3)
+
+        content = re.sub(r"(:)(.*?)(:)", process_match, content)
+        return content
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gitbook2mkdocs",
+    version="0.0.1",
+    description="MkDocs plugin for converting gitbook syntax to mkdocs",
+    keywords="mkdocs markdown gitbook",
+    url="https://github.com/pledra/gitbook2mkdocs/",
+    author="Paul Catinean",
+    author_email="pca@pledra.com",
+    license="MIT",
+    python_requires=">=3.6",
+    install_requires=["mkdocs>=1.1"],
+    packages=find_packages(),
+    entry_points={"mkdocs.plugins": ["gitbook2mkdocs = gitbook2mkdocs.plugin:Gitbook2Mkdocs"]},
+)
